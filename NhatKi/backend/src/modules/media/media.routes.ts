d415fc2701f2614d1d5e2d2b12948339505f55de
ngocanh-dev2209 <iamngocanh2209@gmail.com>
2026-06-12 13:11:30 +0700

    chore(clean): xoa thu muc NhatKi bi long, giu nguyen cau truc cu

--- NhatKi/backend/src/modules/media/media.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { uploadMediaController } from './media.controller';
-
-const mediaRouter = Router();
-mediaRouter.post('/upload', uploadMediaController);
-
-export default mediaRouter;
--- NhatKi/backend/src/realtime/events.ts
+++
@@ -1,8 +0,0 @@
-export const EVENTS = {
-  CONNECT: "connection",
-  DISCONNECT: "disconnect",
-  SHARE_CAPTURE: "share-capture",
-  NEW_CAPTURE: "new-capture",
-  PRESENCE: "presence",
-  ONLINE_LIST: "online-list"
-};
